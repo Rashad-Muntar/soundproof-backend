f5108f2ed422f6e668400cfb5d76b3346bf24dff
Rashad <rashad@Rashads-MacBook-Pro.local>
2023-06-17 15:32:33 -0700

    implemented protected route with middleware check

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,31 +2,90 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/Rashad-Muntar/soundproof/config"
 	"github.com/Rashad-Muntar/soundproof/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func UpdateProfile(c *gin.Context) {
+func Signup(c *gin.Context) {
 	var body struct {
-		Name  string `json:"name" binding:"required"`
-		Email string `json:"email" binding:"required"`
+		Name     string `json:"name" binding:"required"`
+		Email    string `json:"email" binding:"required"`
+		Password string `json:"password" binding:"required"`
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+	bcryptPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password",
+		})
+		return
+	}
+	user := models.User{Name: body.Name, Email: body.Email, Password: string(bcryptPassword)}
+	newUser := config.DB.Create(&user)
+
+	if newUser.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": newUser.Error,
+		})
+		return
+	}
+	c.JSON(200, &user)
+}
+
+func Login(c *gin.Context) {
+	var body struct {
+		Email    string
+		Password string
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
 	}
 	var user models.User
+	config.DB.First(&user, "email = ?", body.Email)
 
-    if err := config.DB.Where("id = ?", c.Param("userId")).First(&user).Error; err != nil {
-        c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "record not found"})
-        return
-    }
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Email is not found",
+		})
+		return
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+		return
+	}
+  
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
 
-    if err := c.BindJSON(&body); err != nil {
-        c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 
-    updatedPost := models.User{Name: body.Name, Email: body.Email}
-    config.DB.Model(&user).Updates(&updatedPost)
-    c.JSON(http.StatusOK, gin.H{"data": user})
-
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+	Bearer := "Bearer " + tokenString
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", Bearer, 3600*24*30, "", "", false, true)
+	c.JSON(200, Bearer)
 }
--- routes/userRoute.go
+++ routes/userRoute.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/Rashad-Muntar/soundproof/controllers"
+	"github.com/Rashad-Muntar/soundproof/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func UserRoute(router *gin.Engine){
+func UserRoute(router *gin.Engine) {
 	router.POST("/auth/signup", controllers.Signup)
 	router.POST("/auth/login", controllers.Login)
-	router.PUT("/auth/:userId", controllers.UpdateProfile)
+	router.PUT("/user/:userId", controllers.UpdateProfile)
+	router.GET("/user/:userId",  middleware.AuthCheck, controllers.GetProfile)
 }
